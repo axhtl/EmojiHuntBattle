1fb185e7fcc6982d8f271596d0a3c4a9f67bbb8f
subin <axhtl@gachon.ac.kr>
2024-12-28 16:16:55 +0900

    first commit

---
+++ src/main/java/com/example/emojihuntbattle/EmojiHuntBattleApplication.java
@@ -0,0 +1,13 @@
+package com.example.emojihuntbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojiHuntBattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmojiHuntBattleApplication.class, args);
+    }
+
+}
